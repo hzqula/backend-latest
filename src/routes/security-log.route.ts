c951fdc1fdc3bfec58ad8862abca71e6693eedb3
M-Adityarnld <aditadalahorang@gmail.com>
2025-05-07 00:43:19 +0700

    refactor(security-log.route): add JWT authentication to cleanup route for enhanced security

--- src/routes/security-log.route.ts
+++ src/routes/security-log.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { authenticateJWT , restrictTo} from "../middlewares/auth";
 import * as securityLogController from "../controllers/security-log.controller";
 const router = express.Router();
 
-router.get("/", securityLogController.getSecurityLogs);
-router.delete("/cleanup", securityLogController.cleanupOldLogs);
+router.get("/",  securityLogController.getSecurityLogs );
+router.delete("/cleanup", securityLogController.cleanupOldLogs , authenticateJWT);
 
 export default router;
