dd879e1add55746dc46927129389bb04b0bff0e4
M-Adityarnld <aditadalahorang@gmail.com>
2025-05-07 00:43:32 +0700

    refactor(student.route): add JWT authentication to student routes for enhanced security

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -3,10 +3,11 @@ import {
   getAllStudents,
   getStudentByNIM,
 } from "../controllers/student.controller";
+import { authenticateJWT , restrictTo} from "../middlewares/auth";
 
 const studentRouter = express.Router();
 
-studentRouter.get("/", getAllStudents);
-studentRouter.get("/:nim", getStudentByNIM);
+studentRouter.get("/",authenticateJWT, getAllStudents); 
+studentRouter.get("/:nim", authenticateJWT, getStudentByNIM);
 
 export default studentRouter;
