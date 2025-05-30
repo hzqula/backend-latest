c0e294a8b170611a7e79988245e28f0d981e5c8c
hzqula <ilooqstrasi@gmail.com>
2025-05-30 10:44:22 +0700

    🎓 change logic of final score (not sum score / total score) but each of score multiplied by each score percentage

--- src/services/seminar.service.ts
+++ src/services/seminar.service.ts
@@ -60,9 +60,13 @@ export class SeminarService {
       if (characteristicScore < 0 || characteristicScore > 100) {
         throw new Error("Guidance score harus antara 0 sampai 100");
       }
-      finalScore = (presentationScore + masteryScore + characteristicScore) / 3;
+      finalScore =
+        presentationScore * 0.25 +
+        masteryScore * 0.4 +
+        characteristicScore * 0.35;
     } else {
-      finalScore = (writingScore + presentationScore + masteryScore) / 3;
+      finalScore =
+        presentationScore * 0.25 + masteryScore * 0.4 + writingScore * 0.35;
     }
     return finalScore;
   }
